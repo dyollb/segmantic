cac70e16d70e01767b947d5e0fcba09630188467
Bryn Lloyd <lloyd@itis.swiss>
2021-09-24 22:22:38 +0200

    use actions/setup-python@v2 and pytest for tests

--- tests/test_prepro_labels.py
+++ tests/test_prepro_labels.py
@@ -7,19 +7,11 @@ from segmantic.prepro import labels
 @pytest.fixture
 def tissue_map() -> dict:
     """maps tissue -> index"""
-    return {"BG": 0, "Bone": 1, "Fat": 2, "Skin": 3}
+    return {"Background": 0, "Bone": 1, "Fat": 2, "Skin": 3}
 
 
-@pytest.fixture
-def tissue2display(tissue_map: dict) -> dict:
-    """maps tissue -> human readable label"""
-    name_map = {key: key.capitalize() for key in tissue_map}
-    name_map["BG"] = "Background"
-    return name_map
-
-
-def test_tissue_list_io(temp_path: Path, tissue_map: dict):
-    file_path = temp_path / "tissue.txt"
+def test_tissue_list_io(tmp_path: Path, tissue_map: dict):
+    file_path = tmp_path / "tissue.txt"
 
     labels.save_tissue_list(tissue_map, file_path)
 
@@ -27,17 +19,24 @@ def test_tissue_list_io(temp_path: Path, tissue_map: dict):
 
     assert len(tissue_map) == len(tissue_dict2)
 
-    tissue_map.pop("BG")
-    assert tissue_map == {k: v for k, v in tissue_dict2.items() if k != "BG"}
+    assert tissue_map == tissue_dict2
 
 
-def test_build_tissue_map(tissue_map: dict, tissue2display: dict):
+def test_build_tissue_map(tissue_map: dict):
+
+    # map "Skin" and "Fat" to "Other_tissue"
+    def map(name):
+        if name == "Background":
+            return name
+        if name == "Bone":
+            return "Bone"
+        return "Other_tissue"
 
     omap, i2o = labels.build_tissue_mapping(tissue_map, map)
     assert len(omap) == 3
 
     for n1 in tissue_map:
-        n2 = tissue2display.get(n1, "Other")
+        n2 = map(n1)
         i1 = tissue_map[n1]
         i2 = omap[n2]
         assert i2o[i1] == i2
