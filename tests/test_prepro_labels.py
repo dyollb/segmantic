c1de13fd372770ebc194a803a7a98fb49e730576
Pedro Crespo <32402063+pcrespov@users.noreply.github.com>
2021-09-24 17:32:59 +0200

    changes testing for pytest

--- tests/test_prepro_labels.py
+++ tests/test_prepro_labels.py
@@ -1,49 +1,43 @@
-# the inclusion of the tests module is not meant to offer best practices for
-# testing in general, but rather to support the `find_packages` example in
-# setup.py that excludes installing the "tests" package
+from pathlib import Path
 
-import unittest
-
-import os
-from tempfile import TemporaryDirectory
-
+import pytest
 from segmantic.prepro import labels
 
 
-class TestPreproLabels(unittest.TestCase):
-    def test_tissue_list_io(self):
-        with TemporaryDirectory() as d:
-            file_path = os.path.join(d, "tissue.txt")
-            tissue_dict = {"BG": 0, "Bone": 1, "Fat": 2, "Skin": 3}
-            labels.save_tissue_list(tissue_dict, file_path)
-
-            tissue_dict2 = labels.load_tissue_list(file_path)
-
-            self.assertEqual(len(tissue_dict), len(tissue_dict2))
-            for n in tissue_dict:
-                if n == "BG":
-                    continue
-                self.assertIn(n, tissue_dict2)
-                self.assertEqual(tissue_dict[n], tissue_dict2[n])
-
-    def test_build_tissue_map(self):
-        tissue_dict = {"BG": 0, "Bone": 1, "Fat": 2, "Skin": 3}
-
-        def map(name):
-            if name == "BG":
-                return "Background"
-            if name == "Bone":
-                return "Bone"
-            return "Other"
-
-        omap, i2o = labels.build_tissue_mapping(tissue_dict, map)
-        self.assertEqual(len(omap), 3)
-        for n1 in tissue_dict:
-            n2 = map(n1)
-            i1 = tissue_dict[n1]
-            i2 = omap[n2]
-            self.assertEqual(i2o[i1], i2)
+@pytest.fixture
+def tissue_map() -> dict:
+    """maps tissue -> index"""
+    return {"BG": 0, "Bone": 1, "Fat": 2, "Skin": 3}
 
 
-if __name__ == "__main__":
-    unittest.main()
+@pytest.fixture
+def tissue2display(tissue_map: dict) -> dict:
+    """maps tissue -> human readable label"""
+    name_map = {key: key.capitalize() for key in tissue_map}
+    name_map["BG"] = "Background"
+    return name_map
+
+
+def test_tissue_list_io(temp_path: Path, tissue_map: dict):
+    file_path = temp_path / "tissue.txt"
+
+    labels.save_tissue_list(tissue_map, file_path)
+
+    tissue_dict2 = labels.load_tissue_list(file_path)
+
+    assert len(tissue_map) == len(tissue_dict2)
+
+    tissue_map.pop("BG")
+    assert tissue_map == {k: v for k, v in tissue_dict2.items() if k != "BG"}
+
+
+def test_build_tissue_map(tissue_map: dict, tissue2display: dict):
+
+    omap, i2o = labels.build_tissue_mapping(tissue_map, map)
+    assert len(omap) == 3
+
+    for n1 in tissue_map:
+        n2 = tissue2display.get(n1, "Other")
+        i1 = tissue_map[n1]
+        i2 = omap[n2]
+        assert i2o[i1] == i2
