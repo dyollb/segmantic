e38c4e91657416d283f2d93252bcaa8a639292a0
Bryn Lloyd <lloyd@itis.swiss>
2022-03-14 13:17:13 +0100

    remove unused code

--- setup.py
+++ setup.py
@@ -1,22 +1,4 @@
-import re
-import sys
-from subprocess import check_output
-from contextlib import suppress
-
 from setuptools import setup
-
-
-def get_cuda_version():
-    with suppress(UnicodeDecodeError, FileNotFoundError):
-        out = check_output(["nvidia-smi"]).decode("utf-8")
-        match = re.search(r"CUDA Version:\s+(\d+).(\d+)", out)
-        if match:
-            version = tuple(int(c) for c in match.groups())
-            print("\nDetected CUDA version: %s.%s\n" % (version[0], version[1]))
-            return version
-
-    print("\nCould not detected CUDA version\n", file=sys.stderr)
-    return (0, 0)
 
 
 if __name__ == "__main__":
