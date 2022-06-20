ceeec99ac88eed26ca94f530003e61d7b838d444
Bryn Lloyd <dyollb@users.noreply.github.com>
2022-06-20 11:46:42 +0200

    🐛 fix intensity augmentation training (#35)
    * fix intensity augmentation training
    
    * update pre-commit hooks
    
    Co-authored-by: Bryn Lloyd <lloyd@itis.swiss>

---
+++ scripts/generate_dataset.py
@@ -0,0 +1,49 @@
+import json
+from pathlib import Path
+
+import typer
+
+from segmantic.util.encoders import PathEncoder
+
+
+def generate_dataset(
+    *,
+    image_dir: Path,
+    labels_dir: Path,
+    image_glob: str = "*.nii.gz",
+    labels_glob: str = "*.nii.gz",
+    dataset_file: Path = None,
+):
+    """Generate json file describing"""
+
+    labels_ext = labels_glob.replace("*", "", 1)
+    labels_file = {
+        p.name.replace(labels_ext, ""): p for p in labels_dir.glob(labels_glob)
+    }
+
+    print(f"Number of label images: {len(labels_file)}")
+
+    image_ext = image_glob.replace("*", "", 1)
+    data_dicts = []
+
+    for p in image_dir.glob(image_glob):
+        key = p.name.replace(image_ext, "")
+        if key in labels_file:
+            data_dicts.append({"image": p, "label": labels_file[key]})
+
+    print(f"Number of pairs: {len(data_dicts)}")
+
+    json_text = json.dumps(
+        {
+            "training": data_dicts,
+        },
+        cls=PathEncoder,
+    )
+    if dataset_file:
+        dataset_file.write_text(json_text)
+    else:
+        print(json_text)
+
+
+if __name__ == "__main__":
+    typer.run(generate_dataset)
