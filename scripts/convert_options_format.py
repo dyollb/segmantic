bd7506c4a1bbda49dadd483d21ecb681833402ea
Bryn Lloyd <lloyd@itis.swiss>
2022-06-22 17:19:18 +0200

    fix script, fix missing stubs for yaml

--- scripts/convert_options_format.py
+++ scripts/convert_options_format.py
@@ -7,17 +7,17 @@ import yaml
 
 def main(input_file: Path, output_file: Path):
     """Convert yml to json and vice versa"""
-    if input_file.suffix == "json":
+    if input_file.suffix == ".json":
         options = json.loads(input_file.read_text())
-    elif input_file.suffix == "yml":
+    elif input_file.suffix == ".yml":
         options = yaml.safe_load(input_file.read_text())
     else:
         raise RuntimeError(f"Cannot read {input_file}. Unsupported file type.")
 
-    if output_file.suffix == "json":
-        input_file.write_text(json.dumps(options))
-    elif output_file.suffix == "yml":
-        input_file.write_text(yaml.safe_dump(options))
+    if output_file.suffix == ".json":
+        output_file.write_text(json.dumps(options))
+    elif output_file.suffix == ".yml":
+        output_file.write_text(yaml.safe_dump(options))
     else:
         raise RuntimeError(f"Cannot read {output_file}. Unsupported file type.")
 
