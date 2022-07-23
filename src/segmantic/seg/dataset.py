709f9760fd278497a7a18d91b612905ee5c84511
Konohana0608 <61539759+Konohana0608@users.noreply.github.com>
2022-07-23 22:03:03 +0200

    ✨added cross-validation capabilities (#46)
    * added cross-validation capabilities
    
    * implemented use of paired datasets for cross validation.
    
    Co-authored-by: Bryn Lloyd <lloyd@itis.swiss>

--- src/segmantic/seg/dataset.py
+++ src/segmantic/seg/dataset.py
@@ -3,6 +3,9 @@ import random
 from pathlib import Path
 from typing import Dict, List, Sequence, Union
 
+import numpy as np
+from sklearn.model_selection import KFold
+
 from ..util.json import PathEncoder
 
 
@@ -34,6 +37,28 @@ def find_matching_files(input_globs: List[Path], verbose: bool = True):
     return output_files
 
 
+def create_data_dict(
+    list_to_convert: List[Dict[str, str]], p: Path, data_dicts: List[Dict[str, Path]]
+):
+    for element in list_to_convert:
+        # special case: absolute paths
+        if Path(element["image"]).is_absolute():
+            image_files = [Path(element["image"])]
+            label_files = [Path(element["label"])]
+        else:
+            image_files = list(p.parent.glob(element["image"]))
+            label_files = list(p.parent.glob(element["label"]))
+        assert len(image_files) == len(label_files)
+
+        for i_element, o_element in zip(
+            sorted(image_files),
+            sorted(label_files),
+        ):
+            data_dicts.append({"image": i_element, "label": o_element})
+
+    return data_dicts
+
+
 class PairedDataSet(object):
     def __init__(
         self,
@@ -47,21 +72,10 @@ class PairedDataSet(object):
         random_seed: int = None,
         max_files: int = 0,
     ):
-        assert image_dir.is_dir() and labels_dir.is_dir()
 
-        if Path(image_glob).is_absolute():
-            image_glob = str(Path(image_glob).relative_to(image_dir))
-        if Path(labels_glob).is_absolute():
-            labels_glob = str(Path(labels_glob).relative_to(labels_dir))
-
-        image_files = list(image_dir.glob(image_glob))
-        label_files = list(labels_dir.glob(labels_glob))
-        assert len(image_files) == len(label_files)
-
-        data_dicts: List[Dict[str, Path]] = []
-        for i, o in zip(sorted(image_files), sorted(label_files)):
-            data_dicts.append({"image": i, "label": o})
-
+        data_dicts = self.create_data_dict(
+            image_dir, image_glob, labels_dir, labels_glob
+        )
         self._create_split(data_dicts, valid_split, shuffle, random_seed, max_files)
 
     def training_files(self) -> Sequence[Dict[str, Path]]:
@@ -72,6 +86,10 @@ class PairedDataSet(object):
         """Get list of 'image'/'label' pairs (dictionary) for validation"""
         return self._val_files
 
+    def test_files(self) -> Sequence[Dict[str, Path]]:
+        """Get list of 'image'/'label' pairs (dictionary) for test"""
+        return self._test_files
+
     def _create_split(
         self,
         data_dicts: List[Dict[str, Path]],
@@ -79,7 +97,9 @@ class PairedDataSet(object):
         shuffle: bool,
         random_seed: int = None,
         max_files: int = 0,
+        test_data_dicts: List[Dict[str, Path]] = [],
     ):
+        self._test_files = test_data_dicts
 
         if shuffle:
             my_random = random.Random(random_seed)
@@ -110,19 +130,67 @@ class PairedDataSet(object):
     def dump_dataset(self) -> str:
         return json.dumps(
             {
-                "training": self._train_files + self._val_files,
-                "split": [len(self._train_files), len(self._val_files)],
+                "training": self._train_files,
+                "validation": self._val_files,
+                "test": self._test_files,
             },
             cls=PathEncoder,
         )
 
     @staticmethod
+    def create_data_dict(
+        image_dir: Path = Path(),
+        image_glob: str = "*.nii.gz",
+        labels_dir: Path = Path(),
+        labels_glob: str = "*.nii.gz",
+    ) -> List[Dict[str, Path]]:
+
+        assert image_dir.is_dir() and labels_dir.is_dir()
+
+        if Path(image_glob).is_absolute():
+            image_glob = str(Path(image_glob).relative_to(image_dir))
+        if Path(labels_glob).is_absolute():
+            labels_glob = str(Path(labels_glob).relative_to(labels_dir))
+
+        image_files = list(image_dir.glob(image_glob))
+        label_files = list(labels_dir.glob(labels_glob))
+        assert len(image_files) == len(label_files)
+
+        data_dicts: List[Dict[str, Path]] = []
+        for i, o in zip(sorted(image_files), sorted(label_files)):
+            data_dicts.append({"image": i, "label": o})
+        return data_dicts
+
+    @staticmethod
+    def kfold_crossval(
+        num_splits: int,
+        data_dicts: List[Dict[str, Path]],
+        output_dir: Path,
+        test_data_dicts: List[Dict[str, Path]] = [],
+    ) -> List:
+        kf = KFold(n_splits=num_splits)
+
+        all_fold_files_dir = output_dir.joinpath("datafolds")
+        all_fold_files_dir.mkdir(exist_ok=True)
+
+        image_idx = np.arange(len(data_dicts))
+        all_dataset_paths: List[Path] = []
+
+        for count, (train_idx, val_idx) in enumerate(kf.split(image_idx, image_idx)):
+            temp_dataset = PairedDataSet()
+            temp_dataset._train_files = [data_dicts[i] for i in train_idx]
+            temp_dataset._val_files = [data_dicts[i] for i in val_idx]
+            temp_dataset._test_files = test_data_dicts
+
+            temp_dataset_path = all_fold_files_dir.joinpath(f"fold_{count}.json")
+            temp_dataset_path.write_text(temp_dataset.dump_dataset())
+            all_dataset_paths.append(temp_dataset_path)
+
+        return all_dataset_paths
+
+    @staticmethod
     def load_from_json(
         file_path: Union[Path, List[Path]],
-        *,
-        valid_split: float = 0.2,
-        shuffle: bool = True,
-        random_seed: int = None,
     ):
         """Loads one or more datasets from json descriptor files and returns a single combined dataset
 
@@ -134,26 +202,35 @@ class PairedDataSet(object):
             "training": [{"image": "image/*.nii.gz", "label": "label/*.nii.gz"}],
         }
         """
+
         if isinstance(file_path, (Path, str)):
             file_path = [file_path]
 
-        data_dicts: List[Dict[str, Path]] = []
+        data_dicts_train: List[Dict[str, Path]] = []
+        data_dicts_val: List[Dict[str, Path]] = []
+        data_dicts_test: List[Dict[str, Path]] = []
 
-        for p in (Path(f) for f in file_path):
-            training = json.loads(p.read_text())["training"]
-            for d in training:
-                # special case: absolute paths
-                if Path(d["image"]).is_absolute():
-                    image_files = [Path(d["image"])]
-                    label_files = [Path(d["label"])]
-                else:
-                    image_files = list(p.parent.glob(d["image"]))
-                    label_files = list(p.parent.glob(d["label"]))
-                    assert len(image_files) == len(label_files)
+        for p in [Path(f) for f in file_path]:
+            ds = json.loads(p.read_text())
+            training = ds["training"]
+            validation = ds["validation"]
+            test = ds["test"] if "test" in ds else []
 
-                for i, o in zip(sorted(image_files), sorted(label_files)):
-                    data_dicts.append({"image": i, "label": o})
+            data_dicts_train = create_data_dict(
+                list_to_convert=training, p=p, data_dicts=data_dicts_train
+            )
+
+            data_dicts_val = create_data_dict(
+                list_to_convert=validation, p=p, data_dicts=data_dicts_val
+            )
+
+            data_dicts_test = create_data_dict(
+                list_to_convert=test, p=p, data_dicts=data_dicts_test
+            )
 
         combined_ds = PairedDataSet()
-        combined_ds._create_split(data_dicts, valid_split, shuffle, random_seed)
+        combined_ds._train_files = data_dicts_train
+        combined_ds._val_files = data_dicts_val
+        if test is not None:
+            combined_ds._test_files = data_dicts_test
         return combined_ds
--- tests/seg/test_dataset.py
+++ tests/seg/test_dataset.py
@@ -9,8 +9,8 @@ def dataset_mockup(
     root_path: Path, label_suffix: str = "", size: int = 3
 ) -> Tuple[Path, Path]:
     image_dir, labels_dir = root_path / "image", root_path / "label"
-    image_dir.mkdir()
-    labels_dir.mkdir()
+    image_dir.mkdir(exist_ok=True)
+    labels_dir.mkdir(exist_ok=True)
 
     for idx in range(size):
         (image_dir / f"img-{idx}.nii.gz").touch()
@@ -37,7 +37,10 @@ def test_PairedDataSet(tmp_path: Path):
 
 
 def test_load_from_json(tmp_path: Path):
-    image_dir, labels_dir = dataset_mockup(root_path=tmp_path, size=3)
+    val_paths = tmp_path.joinpath("val_paths")
+    val_paths.mkdir(exist_ok=True)
+    image_dir, labels_dir = dataset_mockup(root_path=tmp_path, size=2)
+    image_dir_v, labels_dir_v = dataset_mockup(root_path=val_paths, size=1)
 
     dataset_file = tmp_path / "dataset.json"
     dataset_file.write_text(
@@ -45,15 +48,20 @@ def test_load_from_json(tmp_path: Path):
             {
                 "training": [
                     {
-                        "image": f"{image_dir.name}/*.nii.gz",
-                        "label": f"{labels_dir.name}/*.nii.gz",
+                        "image": f"{image_dir.relative_to(tmp_path) / '*.nii.gz'}",
+                        "label": f"{labels_dir.relative_to(tmp_path)/ '*.nii.gz'}",
                     }
-                ]
+                ],
+                "validation": [
+                    {
+                        "image": f"{image_dir_v.relative_to(tmp_path)/'*.nii.gz'}",
+                        "label": f"{labels_dir_v.relative_to(tmp_path)/'*.nii.gz'}",
+                    }
+                ],
             }
         )
     )
-
-    ds = dataset.PairedDataSet.load_from_json(dataset_file, valid_split=0.2)
+    ds = dataset.PairedDataSet.load_from_json(dataset_file)
     assert len(ds.training_files()) == 2
     assert len(ds.validation_files()) == 1
     ds.check_matching_filenames()
@@ -61,10 +69,40 @@ def test_load_from_json(tmp_path: Path):
     # now dump and try to re-load
     dataset_file2 = tmp_path / "dataset_dump.json"
     dataset_file2.write_text(ds.dump_dataset())
-    ds = dataset.PairedDataSet.load_from_json(dataset_file2, valid_split=0.2)
+    ds = dataset.PairedDataSet.load_from_json(dataset_file2)
     assert len(ds.training_files()) == 2
     assert len(ds.validation_files()) == 1
     ds.check_matching_filenames()
+
+
+def test_kfold_crossval(tmp_path: Path):
+    output_dir = tmp_path.joinpath("k_fold_outputs")
+    output_dir.mkdir(exist_ok=True)
+
+    image_dir, labels_dir = dataset_mockup(root_path=tmp_path, size=21)
+    data_dicts = dataset.PairedDataSet.create_data_dict(
+        image_dir=image_dir, labels_dir=labels_dir
+    )
+
+    image_dir, labels_dir = dataset_mockup(root_path=tmp_path, size=3)
+    test_data_dicts = dataset.PairedDataSet.create_data_dict(
+        image_dir=image_dir, labels_dir=labels_dir
+    )
+
+    all_datafold_paths = dataset.PairedDataSet.kfold_crossval(
+        num_splits=7,
+        data_dicts=data_dicts,
+        output_dir=output_dir,
+        test_data_dicts=test_data_dicts,
+    )
+    datafolds_dir = output_dir.joinpath("datafolds")
+    assert len(all_datafold_paths) == 7
+    assert len(sorted(list(datafolds_dir.glob("*.json")))) == 7
+
+    for dpath in all_datafold_paths:
+        assert Path(dpath).is_file()
+        temp_ds = dataset.PairedDataSet.load_from_json(dpath)
+        temp_ds.check_matching_filenames()
 
 
 def test_find_matching_files(tmp_path: Path):
